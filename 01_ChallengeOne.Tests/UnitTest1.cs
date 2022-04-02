32bc15cf07bacdfe5228ececaccdb859f2ac5121
Julia French <jmfrench788@gmail.com>
2022-04-02 09:53:39 -0400

    InitialCommit

---
+++ 01_ChallengeOne.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace _01_ChallengeOne.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ 02_ChallengeTwo_Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace _02_ChallengeTwo_Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
